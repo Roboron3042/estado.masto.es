449d340ad2e4aca5397ec06bddacb54337ae5a43
Roboron3042 <robertoms258@gmail.com>
2024-01-13 12:04:57 +0100

    CHG: Usa sponge en el script en lugar de sed -i

--- status.sh
+++ status.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if curl --fail "https://masto.es"; then
-	sed -i "s/<b>masto.es<\/b>.*/<b>masto.es<\/b>: <span class='green'>Funcionando<\/span>/g" index.html
+	sed "s/<b>masto.es<\/b>.*/<b>masto.es<\/b>: <span class='green'>Funcionando<\/span>/g" index.html | sponge index.html
 else
-	sed -i "s/<b>masto.es<\/b>.*/<b>masto.es<\/b>: <span class='red'>Fallando<\/span>/g" index.html
+	sed "s/<b>masto.es<\/b>.*/<b>masto.es<\/b>: <span class='red'>Fallando<\/span>/g" index.html | sponge index.html
 fi
 
 if curl --fail "https://masto.es/system/accounts/avatars/109/326/388/519/117/627/original/c2702e04351e0b56.jpg" --output avatar.jpg; then
-	sed -i "s/<b>Multimedia<\/b>.*/<b>Multimedia<\/b>: <span class='green'>Funcionando<\/span>/g" index.html
+	sed "s/<b>Multimedia<\/b>.*/<b>Multimedia<\/b>: <span class='green'>Funcionando<\/span>/g" index.html | sponge index.html
 else
-	sed -i "s/<b>Multimedia<\/b>.*/<b>Multimedia<\/b>: <span class='red'>Fallando<\/span>/g" index.html
+	sed "s/<b>Multimedia<\/b>.*/<b>Multimedia<\/b>: <span class='red'>Fallando<\/span>/g" index.html | sponge index.html
 fi
 
-sed -i "s/Última comprobación.*/Última comprobación: $(date)/g" index.html
-sed -i "s/Última actualización.*/Última actualización $(date -r index.js)/g" index.html
+sed "s/Última comprobación.*/Última comprobación: $(date)/g" index.html | sponge index.html
+sed "s/Última actualización.*/Última actualización $(date -r index.js)/g" index.html | sponge index.html
